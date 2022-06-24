b7fb13111c6ec5685a54044bcfbf719015bf75ce
Nonchrono <koochunwoo@gmail.com>
2022-06-24 11:22:37 +0900

    java1_String

---
+++ codingeverybody/src/java1/StringApp.java
@@ -0,0 +1,22 @@
+package java1;
+
+public class StringApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // character (char 문자)
+		System.out.println("H"); //String
+		
+		System.out.println("Hello "
+				+ "World");
+		
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\""); // Hello "World"
+		
+	}
+
+}
