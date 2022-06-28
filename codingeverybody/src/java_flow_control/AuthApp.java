f4d56e0a7b74bf47991bfe895f8a5622e0032d1d
Nonchrono <koochunwoo@gmail.com>
2022-06-28 15:01:03 +0900

    java_flow_control

---
+++ codingeverybody/src/java_flow_control/AuthApp.java
@@ -0,0 +1,23 @@
+package java_flow_control;
+
+public class AuthApp {
+
+	public static void main(String[] args) {
+
+		System.out.println(args[0]);
+		
+		String id = "egoing";
+		String inputId = args[0];
+		
+		System.out.println("Hi.");
+		
+		// if(inputId == id) {
+		if(inputId.equals(id)) {
+			System.out.println("Master!!");
+		} else {
+			System.out.println("Who are you?");
+		}
+		
+	}
+
+}
