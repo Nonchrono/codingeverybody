6005cec82d1f63b36d461dd7134383e3a6a27b59
Nonchrono <koochunwoo@gmail.com>
2022-06-29 12:48:30 +0900

    projectMethod

---
+++ codingeverybody/src/projectMethod/WhyMethod.java
@@ -0,0 +1,24 @@
+package projectMethod;
+
+public class WhyMethod {
+
+	public static void main(String[] args) {
+
+		// 1억번 반복
+		printTwoTimesA();
+		
+		 // 1억번 반복
+		 printTwoTimesA();
+	     
+	     // 1억번 반복
+		 printTwoTimesA();
+		
+	}
+
+	public static void printTwoTimesA() {
+		 System.out.println("-");
+	     System.out.println("a");
+	     System.out.println("a");
+	}
+
+}
