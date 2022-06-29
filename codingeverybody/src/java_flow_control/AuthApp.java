356f6eca7764c283525ce0c23bce690e64382749
Nonchrono <koochunwoo@gmail.com>
2022-06-29 09:47:12 +0900

    java_flow_control

--- codingeverybody/src/java_flow_control/AuthApp.java
+++ codingeverybody/src/java_flow_control/AuthApp.java
@@ -9,15 +9,25 @@ public class AuthApp {
 		String id = "egoing";
 		String inputId = args[0];
 		
+		String pass = "1111";
+		String inputPass = args[1];
+		
 		System.out.println("Hi.");
 		
 		// if(inputId == id) {
-		if(inputId.equals(id)) {
-			System.out.println("Master!!");
-		} else {
-			System.out.println("Who are you?");
-		}
+//		if(inputId.equals(id)) {
+//			if(inputPass.equals(pass)) {
+//				System.out.println("Master!!");
+//			} else {
+//				System.out.println("Who are you?");
+//			} 
+//		}
+		
+		if(inputId.equals(id) && inputPass.equals(pass)) {
+				System.out.println("Master!!");
+			} else {
+				System.out.println("Who are you?");
+			} 
 		
 	}
-
 }
