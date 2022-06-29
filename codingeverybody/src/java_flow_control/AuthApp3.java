247f81d04c8381a8d39e8b4d990699f1dcf520af
Nonchrono <koochunwoo@gmail.com>
2022-06-29 11:13:02 +0900

    java_flow_control

---
+++ codingeverybody/src/java_flow_control/AuthApp3.java
@@ -0,0 +1,35 @@
+package java_flow_control;
+
+public class AuthApp3 {
+
+	public static void main(String[] args) {
+
+		//  String[] users = {"egoing","jinhuck","youbin"};
+		String[][] users = {
+				{"egoing", "1111"},
+				{"jinhuck", "2222"},
+				{"youbin", "3333"}
+		};
+		
+		String inputID = args[0];
+		String inputPass = args[1];
+		
+		boolean isLogined = false;
+		for(int i = 0; i < users.length; i++) {
+			String[] current = users[i];
+			if (
+					current[0].equals(inputID) && 
+					current[1].equals(inputPass)) {
+				isLogined = true;
+				break;
+			}
+		}
+		
+		System.out.println("Hi, ");
+		if(isLogined) {
+			System.out.println("Master!!");
+		} else {
+			System.out.println("Who are you?");
+		}
+	}
+}
