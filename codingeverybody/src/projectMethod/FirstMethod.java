5f15760ac339cb2e01ec06ea5056456b6ac179c3
Nonchrono <koochunwoo@gmail.com>
2022-06-29 12:38:25 +0900

    projectMethod

---
+++ codingeverybody/src/projectMethod/FirstMethod.java
@@ -0,0 +1,14 @@
+package projectMethod;
+
+import java.rmi.server.RMIClassLoaderSpi;
+
+public class FirstMethod {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Method");
+		System.out.println(Math.floor(1.1));
+		
+	}
+
+}
